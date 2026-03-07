90b499dc22f7b029e33879b07e2ab0251875d23e
Selwyn Jesudas <itzselwyn@gmail.com>
2026-03-07 14:15:20 +0530

    changed main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api import weather, pollution, city, traffic
 
-app = FastAPI(title="MetroMind API")
+app = FastAPI()
 
+# Allow frontend to access backend
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Register routers
 app.include_router(weather.router)
 app.include_router(pollution.router)
 app.include_router(city.router)
@@ -10,4 +27,4 @@ app.include_router(traffic.router)
 
 @app.get("/")
 def root():
-    return {"message": "MetroMind backend running"}
+    return {"message": "MetroMind API running"}
